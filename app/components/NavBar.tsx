07dde46e327b25b563fd0396bd942313eab3f6e7
razorhollow <rob@razorhollow.com>
2024-10-23 16:32:47 -0400

    add mobile navigation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { PlusIcon } from '@heroicons/react/20/solid'
-import { Bars3Icon, BellIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Form, NavLink } from '@remix-run/react'
 
 import { useOptionalUser } from '~/utils'
@@ -109,79 +109,19 @@ export default function NavBar() {
                 <div className="space-y-1 pb-3 pt-2">
                     {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
                     <DisclosureButton
-                        as="a"
-                        href="#"
+                        as={NavLink}
+                        to="/calendar"
                         className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
-                    >
-                        Dashboard
-                    </DisclosureButton>
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-                    >
-                        Team
-                    </DisclosureButton>
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-                    >
-                        Projects
-                    </DisclosureButton>
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
                     >
                         Calendar
                     </DisclosureButton>
-                </div>
-                <div className="border-t border-gray-200 pb-3 pt-4">
-                    <div className="flex items-center px-4 sm:px-6">
-                        <div className="flex-shrink-0">
-                            <img
-                                alt=""
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                className="h-10 w-10 rounded-full"
-                            />
-                        </div>
-                        <div className="ml-3">
-                            <div className="text-base font-medium text-gray-800">Tom Cook</div>
-                            <div className="text-sm font-medium text-gray-500">tom@example.com</div>
-                        </div>
-                        <button
-                            type="button"
-                            className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">View notifications</span>
-                            <BellIcon aria-hidden="true" className="h-6 w-6" />
-                        </button>
-                    </div>
-                    <div className="mt-3 space-y-1">
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                        >
-                            Your Profile
-                        </DisclosureButton>
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                        >
-                            Settings
-                        </DisclosureButton>
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                        >
-                            Sign out
-                        </DisclosureButton>
-                    </div>
+                    <DisclosureButton
+                        as={NavLink}
+                        to="/techniques"
+                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                    >
+                        Library
+                    </DisclosureButton>
                 </div>
             </DisclosurePanel>
         </Disclosure>
