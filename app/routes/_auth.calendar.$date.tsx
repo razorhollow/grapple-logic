ac90bd4597e5b63ac26425c64315da9677b092bd
razorhollow <rob@razorhollow.com>
2024-10-24 15:05:54 -0400

    remove debugging logs

--- app/routes/_auth.calendar.$date.tsx
+++ app/routes/_auth.calendar.$date.tsx
@@ -41,8 +41,6 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
         twoMonths
     );
 
-    console.log('reviews', reviews);
-
     return json({
         date: selectedDate,
         isClassDay: true,
